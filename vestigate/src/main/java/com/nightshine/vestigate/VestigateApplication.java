5b80aa237fe06dffe12eb8dfa8b510bd2738323d
sreetejreddy <9701853281mnps@gmail.com>
2020-07-05 16:20:58 +0530

    initial commit - basic project structure

---
+++ vestigate/src/main/java/com/nightshine/vestigate/VestigateApplication.java
@@ -0,0 +1,13 @@
+package com.nightshine.vestigate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VestigateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VestigateApplication.class, args);
+	}
+
+}
---
+++ vestigate/src/test/java/com/nightshine/vestigate/VestigateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nightshine.vestigate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VestigateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
