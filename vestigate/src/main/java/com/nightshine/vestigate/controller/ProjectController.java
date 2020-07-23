9436d783f4a276f0f1077d0c89d7a533dfb26931
prabhakarl <prabhakar.lingala@pramati.com>
2020-07-23 18:46:54 +0530

    refactor : resolved issues

--- vestigate/src/main/java/com/nightshine/vestigate/controller/ProjectController.java
+++ vestigate/src/main/java/com/nightshine/vestigate/controller/ProjectController.java
@@ -85,8 +85,8 @@ public class ProjectController {
     }
 
     @GetMapping("/getAllP")
-    public List<Project> getALL(){
-        return projectService.getAll();
+    public ResponseEntity<List<Project>> getALL(){
+        return ResponseEntity.ok(projectService.getAll());
     }
 
 }
